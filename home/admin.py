43b95a4edd42dd630be618bf2109be6e173f1eaf
dmmonir2003 <dmmonir2003@gmail.com>
2023-12-28 14:16:07 +0600

    create car and brand model

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
+# Register your models here. 
 
-# Register your models here.
+
+admin.site.register(models.CarBrand)
+admin.site.register(models.Car)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
 # Create your models here.
+
+
+
+
+
+     
+
+class CarBrand(models.Model):
+     brand_name=models.CharField(max_length=100)
+     quantity=models.PositiveIntegerField(default=0)
+
+     def __str__(self):
+         return self.brand_name
+     
+
+class Car(models.Model):
+    brand=models.ForeignKey(CarBrand,on_delete=models.CASCADE)
+    car_image=models.ImageField(upload_to='car_images/')
+    name=models.CharField(max_length=100)
+    description=models.TextField()
+    price=models.IntegerField()
+
+    def __str__(self):
+        return self.name
